ff8c9392e826ab9aceb66c80de1d9d812cb1d0bd
rusty-reebs <rusty_reebs@tutanota.com>
2021-10-08 11:30:37 +0400

    modified function and test to include a hit counter and ship symbol

--- src/ship.js
+++ src/ship.js
@@ -2,19 +2,40 @@
 
 //TODO do your best to isolate the app functionality from DOM manipulation
 
-const Ship = (length) => {
-  let hitArray = [];
-  for (i = 0; i < length; i++) {
-    hitArray.push("-");
-  }
-  const hit = (index) => {
-    hitArray.splice(index, 1, "X");
-    return hitArray;
+// add hit to array, if array length matches ship length then sunk
+// add hit to counter, if counter matches length then sunk?
+
+const Ship = (length, symbol) => {
+  this.symbol = symbol;
+  let hitCounter = 0;
+  const hit = () => {
+    hitCounter++;
   };
   const isSunk = () => {
-    return hitArray.every((current) => current == "X");
+    if (hitCounter === length) return true;
   };
-  return { length, hitArray, hit, isSunk };
+  return { hit, isSunk, symbol };
 };
+
+// const carrier = Ship(5, "C");
+// const battleship = Ship(4, "B");
+// const destroyer = Ship(3, "D");
+// const patrolboat = Ship(2, "P");
+
+//old ship
+// const Ship = (length) => {
+//   let hitArray = [];
+//   for (i = 0; i < length; i++) {
+//     hitArray.push("-");
+//   }
+//   const hit = (index) => {
+//     hitArray.splice(index, 1, "X");
+//     return hitArray;
+//   };
+//   const isSunk = () => {
+//     return hitArray.every((current) => current == "X");
+//   };
+//   return { length, hitArray, hit, isSunk };
+// };
 
 module.exports = Ship;

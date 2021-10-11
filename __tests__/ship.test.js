6385e2da5b1eb1a3e6adcef2d0f03df5274335b1
rusty-reebs <rusty_reebs@tutanota.com>
2021-10-11 12:35:01 +0400

    ship.test.js now tests for not sunk with one hit and sunk with all hits

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,42 +2,11 @@
 
 import { Ship } from "../src/ship";
 
-// test("adds a hit", () => {
-//   const battleship = Ship(4);
-//   battleship.hit();
-//   expect(hitCounter).toBe(1);
-// });
-
-// test.skip("battleship symbol is B", () => {
-//   const battleship = Ship(4, "B");
-//   expect(battleship.symbol).toEqual("B");
-// });
-
-test("battleship name is battleship", () => {
-  const battleship = Ship("battleship");
-  expect(battleship.name).toEqual("battleship");
-});
-
-// test("isSunk returns undefined when hits < length", () => {
-//   const battleship = Ship(4, "B");
-//   battleship.hit();
-//   expect(battleship.isSunk()).toBe(undefined);
-// });
-
 test("isSunk returns undefined when hits < length", () => {
   const battleship = Ship("battleship");
   battleship.hit();
   expect(battleship.isSunk()).toBe(undefined);
 });
-
-// test("isSunk returns true when hits = length", () => {
-//   const battleship = Ship(4, "B");
-//   battleship.hit();
-//   battleship.hit();
-//   battleship.hit();
-//   battleship.hit();
-//   expect(battleship.isSunk()).toBe(true);
-// });
 
 test("isSunk returns true when hits = length", () => {
   const battleship = Ship("battleship");
@@ -47,31 +16,3 @@ test("isSunk returns true when hits = length", () => {
   battleship.hit();
   expect(battleship.isSunk()).toBe(true);
 });
-
-// describe("Ship factory tests", () => {
-//   test.skip("adds a hit", () => {
-//     const battleship = Ship(4);
-//   });
-
-//   test("marks position as hit", () => {
-//     const battleship = Ship(4);
-//     const output = ["-", "-", "-", "X"];
-//     expect(battleship.hit(3)).toEqual(output);
-//   });
-
-//   test("isSunk returns false when all indexes != X", () => {
-//     const battleship = Ship(4);
-//     battleship.hit(0);
-//     expect(battleship.isSunk()).toBe(false);
-//   });
-
-//   test("isSunk returns true when all indexes are X", () => {
-//     const battleship = Ship(4);
-//     battleship.hit(0);
-//     battleship.hit(1);
-//     battleship.hit(2);
-//     battleship.hit(3);
-//     battleship.hit(4);
-//     expect(battleship.isSunk()).toBe(true);
-//   });
-// });

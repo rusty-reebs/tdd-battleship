8f649da5ebd791048f83be11091a079006fc94fc
rusty-reebs <rusty_reebs@tutanota.com>
2021-11-02 17:12:25 +0400

    modify webpack settings to build with stylesheet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // ran npm install --save-dev babel-jest @babel/core @babel/preset-env and babel.config.js to use import/export with webpack and jest.
+import "../styles/style.css";
 
 import { gameLoop } from "./gameloop";
 
